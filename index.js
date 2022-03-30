f562c23261994a667e4b023c99d50ab25be4d11f
Ashley Schmalz <100172012+aschmalz95@users.noreply.github.com>
2022-03-30 12:50:34 -0500

    commit

---
+++ index.js
@@ -0,0 +1,74 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !==0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function moveDarkmode(event) {
+        const coins = document.querySelectorAll(".coin");
+        const x = event.clientX * scaleFactor;
+        const y = event.clientY * scaleFactor;
+    
+        for (let i = 0; i < coins.length; ++i) {
+            const isOdd = i % 2 !==0;
+            const boolInt = isOdd ? -1 : 1;
+            coins[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+        }
+}
+
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    email.js
+        .sendForm(
+            "service_ujcyddg",
+            "template_y4sulsg",
+            event.target,
+            "s-N3Itn_V7sgGDmXr"
+        )
+        .then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at ashley.l.schmalz@gmail.com"
+            );
+        })
+}
+
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
